6e57e1958548e89052fb0010e7bb510cb1ca53ec
Thomas <thomas.louarn@gmail.com>
2023-10-17 18:29:39 +0100

    feat: day count conventions

---
+++ disquant/definitions/conventions.py
@@ -0,0 +1,92 @@
+from decimal import Decimal
+from enum import Enum
+
+from disquant.definitions.date import Date, DateRange
+
+
+# Some useful definitions
+# https://www.isda.org/a/pIJEE/The-Actual-Actual-Day-Count-Fraction-1999.pdf
+# https://www.isda.org/a/mIJEE/30-360-2006ISDADefs.xls
+
+
+class DayCount(str, Enum):
+    """
+    Day-count conventions.
+    30E/360: Eurobond Basis
+    30I/360: Bond basis
+    """
+
+    THIRTY_E_360 = "30E/360"
+    THIRTY_360 = "30/360"
+    ACTUAL_360 = "ACT/360"
+    ACTUAL_365_FIXED = "ACT/365F"
+    ACTUAL_ACTUAL_ISDA = "ACT/ACT ISDA"
+
+
+def year_fraction(start: Date, end: Date, day_count: DayCount) -> Decimal:
+    """
+    Compute the fraction of year between two dates.
+    Used to compute the accrued interests on a wide range of financial instruments.
+    """
+    calendar_days = end - start
+
+    match day_count:
+        case DayCount.THIRTY_E_360:
+            """
+            This implementation follows the 30E/360 "Eurobond Basis" definition.
+            2006 ISDA definitions 4.16g
+            """
+            # Cap both start and end date to 30
+            start_day = min(start.day, 30)
+            end_day = min(end.day, 30)
+
+            days = 360 * (end.year - start.year)
+            days += 30 * (end.month - start.month)
+            days += end_day - start_day
+            return Decimal(days) / Decimal(360)
+
+        case DayCount.THIRTY_360:
+            """
+            This implementation follows the 30/360 "Bond basis" definition.
+            2006 ISDA definitions 4.16f
+            - D1 is the first calendar day, expressed as a number, of the Calculation Period
+              or Compounding Period, unless such number would be 31, in which case D1 will be 30
+            - D2 is the calendar day, expressed as a number, immediately following the last day
+              included in the Calculation Period or Compounding Period, unless such number would be
+              31 and D1 is greater than 29, in which case D2 will be 30
+            """
+            start_day = min(start.day, 30)
+            end_day = min(end.day, 30) if start_day > 29 else end.day
+
+            days = 360 * (end.year - start.year)
+            days += 30 * (end.month - start.month)
+            days += end_day - start_day
+            return Decimal(days) / Decimal(360)
+
+        case DayCount.ACTUAL_360:
+            """
+            The actual number of days is always divided by 360.
+            """
+            return Decimal(calendar_days) / Decimal(360)
+
+        case DayCount.ACTUAL_365_FIXED:
+            """
+            The actual number of days is always divided by 365.
+            2006 ISDA definitions 4.16d.
+            
+            Also known as "ACT/365" or "English". 
+            Used in GBP money markets.
+            """
+            return Decimal(calendar_days) / Decimal(365)
+
+        case DayCount.ACTUAL_ACTUAL_ISDA:
+            """
+            This implementation follows the ACT/ACT ISDA definition.
+            https://www.isda.org/a/pIJEE/The-Actual-Actual-Day-Count-Fraction-1999.pdf
+            """
+            leap = sum(1 for x in DateRange(start, end) if x.is_leap)
+            non_leap = calendar_days - leap
+            return Decimal(leap) / Decimal(366) + Decimal(non_leap) / Decimal(365)
+
+        case _:
+            raise NotImplementedError
---
+++ tests/definitions/test_conventions.py
@@ -0,0 +1,109 @@
+from decimal import Decimal
+
+import pytest
+
+from disquant.definitions.conventions import DayCount, year_fraction
+from disquant.definitions.date import Date
+from tests.definitions.test_conventions_data import ISDA_EXAMPLES
+
+
+def test_init():
+    day_count = DayCount("ACT/365F")
+
+    assert day_count == DayCount.ACTUAL_365_FIXED
+
+
+@pytest.mark.parametrize("start_date,end_date,day_count,expected", ISDA_EXAMPLES)
+def test_year_fraction_isda(start_date: Date, end_date: Date, day_count: DayCount, expected: Decimal):
+    assert year_fraction(start_date, end_date, day_count) == expected
+
+
+def test_year_fraction_act_365_fixed():
+    start_date = Date(2023, 9, 18)
+    end_date = Date(2023, 10, 17)
+    day_count = DayCount.ACTUAL_365_FIXED
+
+    fraction = year_fraction(start_date, end_date, day_count)
+
+    assert fraction == Decimal(29) / Decimal(365)
+
+
+def test_year_fraction_actual_actual_isda():
+    """
+    Example taken from
+    https://academybooks.nl/test/Diploma/Book-Interest%20calcs.pdf
+    """
+    start_date = Date(2007, 10, 1)
+    end_date = Date(2008, 4, 1)
+    day_count = DayCount.ACTUAL_ACTUAL_ISDA
+
+    fraction = year_fraction(start_date, end_date, day_count)
+
+    assert fraction == Decimal(92) / Decimal(365) + Decimal(91) / Decimal(366)
+
+
+def test_year_fraction_deltaquants():
+    """
+    Examples taken from:
+    http://www.deltaquants.com/day-count-conventions
+    """
+    # Example 1
+    start = Date(2007, 12, 28)
+    end = Date(2008, 2, 28)
+
+    thirty_360 = year_fraction(start, end, DayCount.THIRTY_360)
+    thirty_e_360 = year_fraction(start, end, DayCount.THIRTY_E_360)
+    act_360 = year_fraction(start, end, DayCount.ACTUAL_360)
+    act_365_fixed = year_fraction(start, end, DayCount.ACTUAL_365_FIXED)
+    act_act_isda = year_fraction(start, end, DayCount.ACTUAL_ACTUAL_ISDA)
+
+    assert thirty_360 == Decimal(60) / Decimal(360)
+    assert thirty_e_360 == Decimal(60) / Decimal(360)
+    assert act_act_isda == Decimal(4) / Decimal(365) + Decimal(58) / Decimal(366)
+    assert act_365_fixed == Decimal(62) / Decimal(365)
+    assert act_360 == Decimal(62) / Decimal(360)
+
+    # Example 2
+    start = Date(2007, 12, 28)
+    end = Date(2008, 2, 29)
+    thirty_360 = year_fraction(start, end, DayCount.THIRTY_360)
+    thirty_e_360 = year_fraction(start, end, DayCount.THIRTY_E_360)
+    act_360 = year_fraction(start, end, DayCount.ACTUAL_360)
+    act_365_fixed = year_fraction(start, end, DayCount.ACTUAL_365_FIXED)
+    act_act_isda = year_fraction(start, end, DayCount.ACTUAL_ACTUAL_ISDA)
+
+    assert thirty_360 == Decimal(61) / Decimal(360)
+    assert thirty_e_360 == Decimal(61) / Decimal(360)
+    assert act_act_isda == Decimal(4) / Decimal(365) + Decimal(59) / Decimal(366)
+    assert act_365_fixed == Decimal(63) / Decimal(365)
+    assert act_360 == Decimal(63) / Decimal(360)
+
+    # Example 3
+    start = Date(2007, 10, 31)
+    end = Date(2008, 11, 30)
+    thirty_360 = year_fraction(start, end, DayCount.THIRTY_360)
+    thirty_e_360 = year_fraction(start, end, DayCount.THIRTY_E_360)
+    act_360 = year_fraction(start, end, DayCount.ACTUAL_360)
+    act_365_fixed = year_fraction(start, end, DayCount.ACTUAL_365_FIXED)
+    act_act_isda = year_fraction(start, end, DayCount.ACTUAL_ACTUAL_ISDA)
+
+    assert thirty_360 == Decimal(390) / Decimal(360)
+    assert thirty_e_360 == Decimal(390) / Decimal(360)
+    assert act_act_isda == Decimal(62) / Decimal(365) + Decimal(334) / Decimal(366)
+    assert act_365_fixed == Decimal(396) / Decimal(365)
+    assert act_360 == Decimal(396) / Decimal(360)
+
+    # Example 4
+    start = Date(2008, 2, 1)
+    end = Date(2009, 5, 31)
+    thirty_360 = year_fraction(start, end, DayCount.THIRTY_360)
+    thirty_e_360 = year_fraction(start, end, DayCount.THIRTY_E_360)
+    act_360 = year_fraction(start, end, DayCount.ACTUAL_360)
+    act_365_fixed = year_fraction(start, end, DayCount.ACTUAL_365_FIXED)
+    act_act_isda = year_fraction(start, end, DayCount.ACTUAL_ACTUAL_ISDA)
+
+    assert thirty_360 == Decimal(480) / Decimal(360)
+    assert thirty_e_360 == Decimal(479) / Decimal(360)
+    assert act_act_isda == Decimal(335) / Decimal(366) + Decimal(150) / Decimal(365)
+    assert act_365_fixed == Decimal(485) / Decimal(365)
+    assert act_360 == Decimal(485) / Decimal(360)
