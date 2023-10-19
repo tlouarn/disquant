8eb02227425899170d274602b73c0bc2523fcf82
Thomas <thomas.louarn@gmail.com>
2023-10-19 12:26:28 +0100

    feat: money object

---
+++ disquant/definitions/money.py
@@ -0,0 +1,59 @@
+from __future__ import annotations
+
+from decimal import Decimal
+from enum import StrEnum
+from functools import total_ordering
+
+
+class Currency(StrEnum):
+    EUR = "EUR"
+    USD = "USD"
+
+
+@total_ordering
+class Money:
+
+    def __init__(self, amount: str | int | Decimal, currency: Currency) -> None:
+        self._amount = Decimal(amount)
+        self._currency = currency
+
+    def __str__(self) -> str:
+        return f"{self.currency} {self.amount}"
+
+    def __repr__(self) -> str:
+        return f"Money(amount={self.amount}, currency={self.currency})"
+
+    @property
+    def amount(self) -> Decimal:
+        return self._amount
+
+    @property
+    def currency(self) -> Currency:
+        return self._currency
+
+    def _check(self, other: Money) -> None:
+        """
+        Check that the other object is of type Money and has the
+        same currency. Raise exceptions if not.
+        """
+        if not isinstance(other, Money):
+            raise TypeError
+
+        if not other.currency == self.currency:
+            raise ValueError
+
+    def __eq__(self, other: Money) -> bool:
+        self._check(other)
+        return other.amount == self.amount
+
+    def __lt__(self, other: Money) -> bool:
+        self._check(other)
+        return self.amount < other.amount
+
+    def __add__(self, other: Money) -> Money:
+        self._check(other)
+        return Money(self.amount + other.amount, self.currency)
+
+    def __sub__(self, other: Money) -> Money:
+        self._check(other)
+        return Money(self.amount - other.amount, self.currency)
---
+++ tests/definitions/test_money.py
@@ -0,0 +1,87 @@
+import pytest
+
+from disquant.definitions.money import Currency, Money
+
+
+def test_init():
+    money = Money(10_000_000, Currency.EUR)
+
+    assert money.amount == 10_000_000
+    assert money.currency == Currency.EUR
+
+
+def test_eq():
+    amount_1 = Money(10_000_000, Currency.EUR)
+    amount_2 = Money(10_000_000, Currency.EUR)
+
+    assert amount_1 is not amount_2
+    assert amount_1 == amount_2
+
+
+def test_lt():
+    amount_1 = Money(9_000_000, Currency.EUR)
+    amount_2 = Money(10_000_000, Currency.EUR)
+
+    assert amount_1 < amount_2
+
+
+def test_total_ordering():
+    amount_1 = Money(9_000_000, Currency.EUR)
+    amount_2 = Money(10_000_000, Currency.EUR)
+    amount_3 = Money(10_000_000, Currency.EUR)
+
+    assert amount_1 < amount_2
+    assert amount_2 <= amount_3
+    assert amount_2 == amount_3
+    assert amount_2 > amount_1
+    assert amount_3 >= amount_2
+    assert amount_3 > amount_1
+    assert amount_1 != amount_3
+
+
+def test_sort():
+    asc_amounts = []
+    for i in range(1, 10):
+        asc_amounts.append(Money(i * 1_000_000, Currency.EUR))
+
+    desc_amounts = sorted(asc_amounts.copy(), reverse=True)
+
+    assert sorted(desc_amounts) == asc_amounts
+
+
+def test_different_currency_raises_exception():
+    amount_eur = Money(10_000_000, Currency.EUR)
+    amount_usd = Money(10_000_000, Currency.USD)
+
+    with pytest.raises(ValueError):
+        assert amount_eur == amount_usd
+
+
+def test_add():
+    amount_1 = Money(9_000_000, Currency.EUR)
+    amount_2 = Money(10_000_000, Currency.EUR)
+
+    assert amount_1 + amount_2 == Money(19_000_000, Currency.EUR)
+
+
+def test_invalid_add():
+    amount_1 = Money(9_000_000, Currency.EUR)
+    amount_2 = Money(10_000_000, Currency.USD)
+
+    with pytest.raises(ValueError):
+        amount_3 = amount_1 + amount_2
+
+
+def test_sub():
+    amount_1 = Money(9_000_000, Currency.EUR)
+    amount_2 = Money(10_000_000, Currency.EUR)
+
+    assert amount_1 - amount_2 == Money(-1_000_000, Currency.EUR)
+
+
+def test_invalid_sub():
+    amount_1 = Money(9_000_000, Currency.EUR)
+    amount_2 = Money(10_000_000, Currency.USD)
+
+    with pytest.raises(ValueError):
+        amount_3 = amount_1 - amount_2
